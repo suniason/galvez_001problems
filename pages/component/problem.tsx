a589755bf8e1746deb8def07682c248ab5ca7fd0
suniason <galvezbrett17@gmail.com>
2023-02-02 23:44:41 +0800

    new commit

--- pages/component/problem.tsx
+++ pages/component/problem.tsx
@@ -9,6 +9,18 @@ const Problem: React.FC = () => {
                         <h1 className="problemhead">PROBLEM</h1>
                     </div>
                     <div className="content">
+                        In the present times, people have been fighting against fake new, misinformation and disinformation.
+                        However, the threat seemed to become worse. According to a statistical data from Statista.com, 
+                        approximately 50% of consumers from each region witnesses false information about COVID-19. 
+                        <div>
+                        <img src="statistics.png" 
+                        alt="Consumers witnessing false information on certain topics worldwide 2022 by region" />
+                        </div>
+                        That is very dangerous because in times of crisis, this information could mislead people to doing 
+                        harmful actions. Things like the anti-vax movement where people instead take on the risky route of 
+                        not getting vaccinated because of a random post on social media that vaccination is more dangerous 
+                        than the virus itself. It is insane how people are able to distrust science because of a post 
+                        that is targeted to strike fear against a very good cause.
                     </div>
                 </div>
         </div>
--- pages/component/solution.tsx
+++ pages/component/solution.tsx
@@ -12,8 +12,17 @@ const Solution : React.FC = () => {
                           
                         </span>
                         <span className="solutionspan">
+
                         </span>
                     </div>
+                </div>
+                <div className="content">
+                    Information technology gives us easier sharing capablities of reliable information but it also 
+                    makes it easier to spread wrong or false information. To fight 
+                    against fake news using it, we can create an AI or Machine Learning system that gathers data 
+                    from various reliable sources to check the validity of the information. Then the system can run  
+                    an algorithm that can help limit the sharing of fake news. This can either be created into an app 
+                    or integrated into various platforms such as social media. This
                 </div>
             </div>
         </div>
