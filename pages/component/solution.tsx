a481ec5beae3e9542f98a908f8d0e1f698641ee3
suniason <galvezbrett17@gmail.com>
2023-02-03 10:42:46 +0800

    probably final commit

--- pages/component/solution.tsx
+++ pages/component/solution.tsx
@@ -23,8 +23,11 @@ const Solution : React.FC = () => {
                     We can create an AI or Machine Learning system that gathers data 
                     from various reliable sources to check the validity of the information. Then the system can run  
                     an algorithm that can help limit the sharing of fake news. This can either be created into an app 
-                    or integrated into various platforms such as social media. 
-                    <br /><br />
+                    or integrated into various platforms such as social media.
+                    <br/><br/>
+                    To sum up, we can use information technology to to our favor. It is a key driving force in ensuring that 
+                    the information we receive and disseminate is reliable and trustworthy and make sure. With it, we can 
+                    restrain the spread of false information and create a safer environment.
 
                 </div>
             </div>
