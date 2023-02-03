1d0daeb67bb89162ee29fe1be1a6f6bfaa21bde1
suniason <galvezbrett17@gmail.com>
2023-02-03 09:36:01 +0800

    another commit

--- pages/component/solution.tsx
+++ pages/component/solution.tsx
@@ -17,12 +17,15 @@ const Solution : React.FC = () => {
                     </div>
                 </div>
                 <div className="content">
-                    Information technology gives us easier sharing capablities of reliable information but it also 
-                    makes it easier to spread wrong or false information. To fight 
-                    against fake news using it, we can create an AI or Machine Learning system that gathers data 
+                    Information technology gives us easier sharing capablities of facts but it also 
+                    makes it easier to spread wrong or false information. To use information technology 
+                    against fake news, we can take advantage of the technologically advanced systems.
+                    We can create an AI or Machine Learning system that gathers data 
                     from various reliable sources to check the validity of the information. Then the system can run  
                     an algorithm that can help limit the sharing of fake news. This can either be created into an app 
-                    or integrated into various platforms such as social media. This
+                    or integrated into various platforms such as social media. 
+                    <br /><br />
+
                 </div>
             </div>
         </div>
